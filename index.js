aefda6d0352045cc28ce21d9aaac18e9e66402c1
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-03-25 19:41:21 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(driver,query){
+    return driver.filter(driver =>
+        driver.toLowerCase() === query.toLowerCase());
+     }
+    
+function fuzzyMatch(driver,letters){
+    return driver.filter(driver =>
+        driver.toLowerCase().startsWith(letters.toLowerCase()));
+    
+    }
+
+function matchName(driver,name){
+
+    return driver.filter(driver =>
+        driver.name.toLowerCase() === name.toLowerCase());
+      
+    }
